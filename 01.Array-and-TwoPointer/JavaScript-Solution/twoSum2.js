d693e4d26cba29cb93d53de40015f03c5c49b410
Khush0031 <chaurasiakhush0031@gmail.com>
2024-02-20 00:14:58 +0530

    adding javaScript Solution

---
+++ 01.Array-and-TwoPointer/JavaScript-Solution/twoSum2.js
@@ -0,0 +1,14 @@
+var twoSum = function(numbers, target) {
+    let n= numbers.length;
+    let i=0;
+    let j= n-1;
+    while(i<j){
+        if(numbers[i]+numbers[j]==target){
+            return [i+1,j+1];
+        }else if(numbers[i]+numbers[j]>target){
+            j--;
+        }else {
+            i++;
+        }
+    }
+};
