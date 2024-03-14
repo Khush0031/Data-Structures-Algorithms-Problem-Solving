2ab9444ce08cfe64988ad5953d8897fa34dfa3c9
Khush0031 <chaurasiakhush0031@gmail.com>
2024-03-14 11:52:20 +0530

    Adding C++ Solution

--- 01.Array-and-TwoPointer/CPP-twoPointer-Solution/movesZeroesToEnd.cpp
+++ 01.Array-and-TwoPointer/CPP-twoPointer-Solution/movesZeroesToEnd.cpp
@@ -1,21 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(vector<int> &nums)
+    {
         int n = nums.size();
-        int i=0;
-        int j= 0;
-        while(j<n){
-            if(nums[j] != 0){
+        int i = 0;
+        int j = 0;
+        while (j < n)
+        {
+            if (nums[j] != 0)
+            {
                 nums[i] = nums[j];
                 i++;
             }
             j++;
         }
-        for(int k=i;k<n;k++){
-            nums[k]=0;
+        for (int k = i; k < n; k++)
+        {
+            nums[k] = 0;
+        }
+    }
+};
+
+// Second method.
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+
+        while (j < n)
+        {
+            if (nums[j] == 0)
+            {
+                j++;
+                continue;
+            }
+            else
+            {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
         }
     }
 };
